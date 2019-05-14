3f0811f2d2a24a7c73ff9e7335a8bcc56f200f81
Hammad <hammadarshad834@gmail.com>
2019-05-15 01:23:02 +0500

    Added E6

---
+++ Exercises/E6-MarkSheet.py
@@ -0,0 +1,17 @@
+''' This marksheet based on 5 subjects if you want to add
+more so, you can easily add more by this method '''
+
+#taking input of all subjects from user
+physics = int(input('Enter the marks of Physics: '))
+maths = int(input('Enter the marks of Maths: '))
+english = int(input('Enter the marks of English: '))
+urdu = int(input('Enter the marks of Urdu: '))
+chemistry = int(input('Enter the marks of Chemistry: '))
+
+#this condition is for making our output more efficient
+if((physics < 0 and physics >100) or (maths < 0 and maths >100) or (urdu < 0 and urdu >100) or (english < 0 and english >100) or (chemistry < 0 and chemistry > 100)):
+    print('Invalid input')
+else:
+    obtainedMarks = physics + maths + english + urdu + chemistry
+    percentage = (obtainedMarks * 100) / 500
+    print('Marks obtained = ' + str(obtainedMarks) + '/500','percentage = ' + str(percentage) + '%',sep='\n')
