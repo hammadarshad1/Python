fc2a9e11fb93b9643a75c930680806d48d55165a
Muhammad Hammad <45298916+hammad7771@users.noreply.github.com>
2019-05-15 22:41:59 -0700

    Update E1-print twinkle twinkle.py

--- Exercises/E1-print twinkle twinkle.py
+++ Exercises/E1-print twinkle twinkle.py
@@ -1,3 +1,8 @@
+# For printing something in python you need to write function print()
+# and put the string inside the "" or '' but if you want to put integer or Float 
+# on it simply just write print function and put the value inside it 
+# without quotes like: print(5) or print(4.5)
+
 print('Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n'
       '\t\tUp above the world so high,\n\t\tLike a diamond in the sky\n'
       'Twinkle, twinkle, little star\n\tHow I wonder what you are')
